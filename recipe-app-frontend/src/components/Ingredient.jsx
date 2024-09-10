ef97181548422b38549264063360c4b7386deee8
Hamzi Haidar <hamzihaider@gmail.com>
2024-09-10 11:13:42 +0300

    added edit recipe feature and fixed bugs

--- recipe-app-frontend/src/components/Ingredient.jsx
+++ recipe-app-frontend/src/components/Ingredient.jsx
@@ -1,13 +1,15 @@
 import { HiX } from "react-icons/hi";
 
-function Ingredient({ ing, setIngredients }) {
+function Ingredient({ ing, setIngredients, show }) {
   return (
     <div className="relative h-20 min-w-40 rounded-2xl bg-orange-400 p-5 pr-10 text-white">
-      {setIngredients && (
+      {setIngredients && !show && (
         <button
-          onClick={() =>
-            setIngredients((ings) => ings.filter((_, i) => i !== ing.id))
-          }
+          id={ing.id}
+          onClick={(e) => {
+            e.preventDefault();
+            setIngredients((ings) => ings.filter((_, i) => i !== ing.id));
+          }}
           className="absolute right-3 top-2"
         >
           <HiX size="1.2rem" />
